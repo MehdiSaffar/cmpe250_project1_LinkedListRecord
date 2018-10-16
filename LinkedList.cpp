1fd462c815e8801d9297916b7a7ddc221b3e2f88
MehdiSaffar <saffar.mehdi3@gmail.com>
2018-10-16 16:12:19 +0300

    Added method definitions to .cpp files

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -1,3 +1,34 @@
 #include "LinkedList.h"
 
 
+LinkedList::LinkedList() {
+
+}
+
+LinkedList::LinkedList(const LinkedList &list) {
+
+}
+
+LinkedList &LinkedList::operator=(const LinkedList &list) {
+    return <#initializer#>;
+}
+
+LinkedList::LinkedList(LinkedList &&list) {
+
+}
+
+LinkedList &LinkedList::operator=(LinkedList &&list) {
+    return <#initializer#>;
+}
+
+void LinkedList::pushTail(string _name, float _amount) {
+
+}
+
+void LinkedList::updateNode(string _name, float _amount) {
+
+}
+
+LinkedList::~LinkedList() {
+
+}
